6174cb531b74b3bae07cc6e3c428c2a62b68a648
BlackWerf1257 <mklsjh44@gmail.com>
2024-11-08 01:06:15 +0900

    갤러리 기능 구현중

--- Assets/Script/Start/StartManage.cs
+++ Assets/Script/Start/StartManage.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+#if !UNITY_EDITOR && UNITY_ANDROID
+using( AndroidJavaClass ajc = new AndroidJavaClass( "com.yasirkula.unity.NativeGalleryMediaPickerFragment" ) )
+	ajc.SetStatic<bool>( "GrantPersistableUriPermission", true );
+#endif
+using System.IO;
 
 public class StartManage : MonoBehaviour
 {
    [SerializeField] private ScrollRect creditsObj;
    [SerializeField] private GameObject parentObj;
    
+   
+   
    public void StartEvent(int idx)
    {
       switch (idx)
       {
          case 0: SceneManager.LoadScene("Main_Cam"); break;
-         case 1: SceneManager.LoadScene("Main_Gallery"); break;
+         case 1:
+         {
+               ;
+            //SceneManager.LoadScene("Main_Gallery");
+
+         } break;
       }
    }
    public void Quit() => Application.Quit();
@@ -29,6 +41,32 @@ public class StartManage : MonoBehaviour
       {
          parentObj.SetActive(true);
          creditsObj.gameObject.SetActive(false);   
+         
       }
    }
+   
+   
+   #region Gallery
+
+   void CheckPermission()
+   {
+      //
+      NativeGallery.Permission permissions = new NativeGallery.Permission();
+      if (permissions == NativeGallery.Permission.Granted)
+         Debug.Log("Permission granted");
+      //NativeGallery.Permission permission = 
+   }
+
+   void LoadFile()
+   {
+      NativeGallery.GetImageFromGallery(file =>
+      {
+         FileInfo fileInfo = new FileInfo(file);
+         //용량 제한
+         if (fileInfo.Length > 10) return;
+         
+      });
+   }
+
+   #endregion
 }
